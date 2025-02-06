ea81b9030192df792eae8ecbe4b4262556d5a2a5
JoaoCardosoDev <jmcardoso90@gmail.com>
2025-02-06 19:57:02 +0000

    chore: folder

---
+++ session1/models.js
@@ -0,0 +1,22 @@
+const profile = {
+    name: "",
+    email: "",
+    age: 0,
+    mailingList: true,
+}
+
+const post = {
+    title: "",
+    body: "",
+    date: "",
+    commentsActive: true,
+}
+
+const comments = {
+    user: "",
+    body: "",
+    date: "",
+    visible: true,
+}
+
+
